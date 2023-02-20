ef32bccacee923a699acc31dd770683904f11ea1
megamindddd <64695437+megamindddd@users.noreply.github.com>
2023-02-20 15:06:59 +0700

    Update BE for manage Studio

---
+++ fitnessbackend/src/main/java/fivemonkey/com/fitnessbackend/services/StudioService.java
@@ -0,0 +1,48 @@
+package fivemonkey.com.fitnessbackend.services;
+
+import fivemonkey.com.fitnessbackend.entitties.Studio;
+import fivemonkey.com.fitnessbackend.repository.StudioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudioService {
+
+    @Autowired
+    StudioRepository studioRepository;
+
+    public List<Studio> getStudios(){
+        return studioRepository.findAll();
+    }
+
+    public boolean addStudio(Studio studio){
+        return studioRepository.save(studio) !=null;
+    }
+    public void insertStudio(Studio studio){
+        studioRepository.save(studio);
+    }
+    public void deleteStudio(Long id){
+        studioRepository.deleteById(id);
+    }
+
+    public List<Studio> findStudio(String studioCity){
+        return studioRepository.findStudio(studioCity);
+    }
+    public List<Studio> findStudioCity(@Param("city") String studioCity){
+        return studioRepository.findStudiosByStudioCity(studioCity);
+    }
+//    public Studio updateStudio(Studio studio){
+//        Studio s = studioRepository.findById(studio.getStudioID()).orElse(null);
+//        s.setStudioCity(studio.getStudioCity());
+//        s.setStudioDistrict(studio.getStudioDistrict());
+//        s.setStudioRoad(studio.getStudioRoad());
+//        s.setStudioName(studio.getStudioName());
+//        s.setStudioContact(studio.getStudioContact());
+//        s.setStudioDescription(studio.getStudioDescription());
+//        return studioRepository.save(s);
+//    }
+
+}
