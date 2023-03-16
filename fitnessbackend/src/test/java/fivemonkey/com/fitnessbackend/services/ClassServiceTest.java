afa778dbfccd8aa3885e4851ddb932bfb8f74911
nguyenvanduc1412000 <ducnvhe141646@fpt.edu.vn>
2023-03-16 17:16:19 +0700

    testing mockito

---
+++ fitnessbackend/src/test/java/fivemonkey/com/fitnessbackend/services/ClassServiceTest.java
@@ -0,0 +1,45 @@
+package fivemonkey.com.fitnessbackend.services;
+
+import fivemonkey.com.fitnessbackend.dto.ClassDTO;
+import fivemonkey.com.fitnessbackend.entities.Clazz;
+import fivemonkey.com.fitnessbackend.entities.Trainer;
+import fivemonkey.com.fitnessbackend.repository.ClassRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest
+class ClassServiceTest {
+    @InjectMocks
+    private ClassService classService;
+
+    @Mock
+    private ClassRepository classRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+        List<ClassDTO> listClass=  classService.findAll();
+        Assertions.assertThat(listClass).isNotNull();
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+}
--- fitnessbackend/src/test/java/fivemonkey/com/fitnessbackend/services/impl/TrainerServiceImplTest.java
+++ fitnessbackend/src/test/java/fivemonkey/com/fitnessbackend/services/impl/TrainerServiceImplTest.java
@@ -1,21 +1,42 @@
 package fivemonkey.com.fitnessbackend.services.impl;
 
+import fivemonkey.com.fitnessbackend.controller.TrainerController;
+import fivemonkey.com.fitnessbackend.entities.Trainer;
+import fivemonkey.com.fitnessbackend.repository.TrainerRepository;
+import fivemonkey.com.fitnessbackend.services.TrainerService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
-@ExtendWith(MockitoExtension.class)
-class TrainerServiceImplTest {
 
-    
+class TrainerServiceImplTest {
+    @Mock
+    TrainerService trainerService;
+     @InjectMocks
+     private TrainerController trainerController;
+
+
+     TrainerRepository trainerRepository;
+
+
+
     @BeforeEach
     void setUp() {
+
     }
 
     @AfterEach
@@ -24,5 +45,11 @@ class TrainerServiceImplTest {
 
     @Test
     void getAll() {
+      List<Trainer> listTrainer=  trainerRepository.findAll();
+      Assertions.assertThat(listTrainer).isNotNull();
+    }
+    @Test
+    void getTrainerByEmail(){
+
     }
 }
